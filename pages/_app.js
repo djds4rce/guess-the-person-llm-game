888acbe63edf6205b0949e791e32b811ab3e4452
Dayanand Prabhu <djd4rce@gmail.com>
2025-02-02 11:33:06 +0000

    fixed image path

--- pages/_app.js
+++ pages/_app.js
@@ -15,11 +15,11 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="robots" content="index, follow"/>
         <meta property="og:title" content="AI Charades, the AI guessing game"/>
         <meta property="og:description" content="Unmask the mystery! Play 'Guess the Personality,' an AI-powered guessing game. Interrogate a famous persona, uncover clues, and deduce their secret identity."></meta>
-        <meta property="og:image" content="./images/aicharades.jpg"/>
+        <meta property="og:image" content="/images/aicharades.jpg"/>
         <meta property="og:url" content="https://aicharades.in/"/>
         <meta name="twitter:title" content="AI Charades, the AI guessing game"/>
         <meta name="twitter:description" content="Unmask the mystery! Play 'Guess the Personality,' an AI-powered guessing game. Interrogate a famous persona, uncover clues, and deduce their secret identity."/>
-        <meta name="twitter:image" content="./images/aicharades.jpg"/>
+        <meta name="twitter:image" content="/images/aicharades.jpg"/>
         <meta name="twitter:card" content="summary_large_image"/>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
